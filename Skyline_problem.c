fc90fed5d11c554e63223ccfdc3e27db15c856fc
tteixeir <96447538+tteixeir@users.noreply.github.com>
2021-12-29 20:14:47 +0000

    Update and rename Skyline_problem to Skyline_problem.c

--- Skyline_problem.c
+++ Skyline_problem.c
@@ -4,24 +4,8 @@
 
 
 #define lim 10000
-#define lim_builds 5000
+#define lim_builds 7
 #define n 3
-
-// conferir tamanho de edificios.
-
-int max_size(int analise){
-	int builds[n][n];
-	mapbuilds(builds);
-	int *p;
-	int g;
-	int h;
-	for (g=0;g<lim_builds;++g){ 
-			h = 0;
-			for(h=0;h<n;++h){
-							
-		}
-	}
-}
 
 //mapear edificios
 
@@ -29,6 +13,8 @@ int mapbuilds(int builds[n][n]){
 int n_builds;
 int i;
 int a;
+int analise = 0;
+int erro = 0;
 
 	for (n_builds = 0; n_builds < lim_builds ; n_builds++){
 	
@@ -39,24 +25,35 @@ int a;
 			n_builds++;
 			for(a=0;a<n;++a){
 				scanf("%d",&builds[i][a]);
+				
 			}
 		}
 		// escreve no ecrã uma o vetor.
 		
 			for (i=0;i<lim_builds;++i){
-				printf("\n");
+			//	printf("\n");
 					for(a=0;a<n;a++){
-						printf("%d",builds[i][a]);
+					//	printf("%d",builds[i][a]);
+						
+						// analise de tamanho dos edificios.                // a impressão tá desabilitada 
+						analise = builds[i][a];
+							if (analise > lim){
+							//	printf(" erro ");
+								erro ++;
+							}else{
+							//	printf(" tudo bem ");
+						}
 					}
 			}	
 	}	
 		// escreve o numero de edificios.
-		printf("\n");
-		printf("Numero de edificios: %d",n_builds);
+	//	printf("\n");
+	//	printf("Numero de edificios: %d",n_builds);
+		if (erro > 0){
+			printf("\n");
+			printf (" edificio/s fora dos limites.");
+		}
 }
-
-
-	
 
 
 int main (){
@@ -65,8 +62,5 @@ int main (){
 	int analise;
 	int builds[n][n];
 	mapbuilds(builds);
-	max_size(analise);
-	
 	
 }	
-
